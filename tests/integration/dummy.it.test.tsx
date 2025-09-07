8fdacbf6b1de780e4d8750f4d3ae92ac85ce6a05
SeriousBug98 <138648012+SeriousBug98@users.noreply.github.com>
2025-09-07 14:19:41 +0900

    Revert "Feature/ci (#2)" (#3)
    This reverts commit 5b40e1a1226d1d50ea118e0b7e3230dd5d9ad346.

--- tests/integration/dummy.it.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-function Hello() {
-    return <h1>Hi</h1>;
-}
-test("renders", () => {
-    render(<Hello />);
-    expect(screen.getByText("Hi")).toBeInTheDocument();
-});
