d149f1eb3ccedf3fe563fb22cadf67fcd2cb47a0
SeriousBug98 <martin1156@konkuk.ac.kr>
2025-10-05 19:46:24 +0900

    fix: 수정

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,7 +8,7 @@ AWS_REGION="$(grep '^AWS_REGION=' .env | cut -d= -f2)"
 
 # ECR 로그인
 aws ecr get-login-password --region "$AWS_REGION" \
- | sudo docker login --username AWS --password-stdin "$(echo "$APP_IMAGE" | awk -F: '{print $1}')"
+ | docker login --username AWS --password-stdin "$(echo "$APP_IMAGE" | awk -F: '{print $1}')"
 
 # 최신 이미지 풀
 sudo docker pull "$APP_IMAGE"
